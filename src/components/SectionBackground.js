55ec439e680c1ebc32e26babc8ed1ee1eb38e512
MrJoey-GloryDoge <hello@glorydogecoin.com>
2021-10-04 17:41:39 +0200

    Fix section background

--- src/components/SectionBackground.js
+++ src/components/SectionBackground.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 const SectionBackground = () => {
   return (
-    <div className="absolute h-[600px] -left-10 -right-10 top-0 bg-gradient-to-b from-secondary  to-transparent opacity-5 -skew-y-3" />
+    <div className="absolute h-[600px] left-0 right-0 top-0 bg-gradient-to-b from-secondary  to-transparent opacity-5 -skew-y-3" />
   )
 }
 
