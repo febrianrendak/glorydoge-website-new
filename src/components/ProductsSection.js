2e0a261c3d124dc7d075a2ce9a18256d34e8acd3
MrJoey-GloryDoge <hello@glorydogecoin.com>
2021-10-04 18:25:31 +0200

    Fix buttons style

--- src/components/ProductsSection.js
+++ src/components/ProductsSection.js
@@ -5,7 +5,7 @@ import SectionBackground from './SectionBackground'
 
 const ProductsSection = () => {
   return (
-    <Container className="relative mt-28 pt-20">
+    <Container className="relative mt-36 pt-20">
       <SectionBackground />
       <div className="flex flex-col items-center">
         <h2 className="text-4xl leading-normal w-auto font-bold text-gradient">GloryPush</h2>
