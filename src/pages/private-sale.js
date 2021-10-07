33817c51157608b2e8f5fab7d4b133d6b5678707
MrJoey-GloryDoge <hello@glorydogecoin.com>
2021-10-08 01:32:53 +0200

    Update

--- src/pages/private-sale.js
+++ src/pages/private-sale.js
@@ -6,6 +6,12 @@ const PrivateSalePage = () => {
   return (
     <>
       <NavBar />
+      <div className="flex items-center justify-center mt-60 w-full">
+        <h1 className="text-gradient text-4xl font-bold leading-normal">
+          Private sale will begin soon. <br />
+          Join us on Telegram to keep updated.
+        </h1>
+      </div>
     </>
   )
 }
