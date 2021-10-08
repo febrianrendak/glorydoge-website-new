01973340c42faa0b0be852a7fbf5c126809ededc
MrJoey-GloryDoge <hello@glorydogecoin.com>
2021-10-08 03:06:24 +0200

    Fix small text issue

--- src/components/NewsBox.js
+++ src/components/NewsBox.js
@@ -7,7 +7,7 @@ const NewsBox = () => {
     <Container className="pt-12 sm:pt-32">
       <div className="h-[400px] relative z-0 w-full bg-black bg-opacity-30 rounded-lg shadow-inner overflow-hidden">
         <div className="t-0 z-[-1] md:w-[45%] absolute flex flex-col items-center justify-center px-6 h-32 text-center pointer-events-none space-y-3 sm:px-20 md:h-full">
-          <h3 className="text-gradient text-xl font-bold leading-normal lg:text-4xl xl:text-5xl">
+          <h3 className="text-gradient line-height-normal text-xl font-bold lg:text-4xl xl:text-5xl">
             Stay up to date
           </h3>
           <p className="text-sm lg:text-base">
