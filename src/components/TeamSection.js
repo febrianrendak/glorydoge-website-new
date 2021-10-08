15123e86fedbb26e4d732ca5b63428e26cd66e44
MrJoey-GloryDoge <hello@glorydogecoin.com>
2021-10-08 12:55:50 +0200

    Replace team names with TG handles

--- src/components/TeamSection.js
+++ src/components/TeamSection.js
@@ -13,33 +13,33 @@ const TeamSection = () => {
       <div className="grid gap-y-20 grid-cols-1 mx-auto my-24 sm:my-32 sm:w-80 lg:gap-x-14 lg:grid-cols-3 lg:w-10/12">
         <div className="flex flex-col items-center">
           <StaticImage
-            src="../images/joey.png"
-            quality={90}
-            className="w-[150px] mb-4"
-            alt=""
-            placeholder="none"
-          />
-          <h3 className="text-xl font-bold">Mr. Joey</h3>
-          <p className="text-gradient font-bold">CEO & Lead Dev</p>
-          <p className="mt-4 text-center">
-            Responsible for building, testing, and maintaining code for the contracts and the
-            upcoming platforms. Been through a successful career as a software engineer since 2014.
-          </p>
-        </div>
-
-        <div className="flex flex-col items-center">
-          <StaticImage
             src="../images/william.png"
             quality={90}
             className="w-[150px] mb-4"
             alt=""
             placeholder="none"
           />
-          <h3 className="text-xl font-bold">Mr. William</h3>
+          <h3 className="text-xl font-bold">@GloryDogeMarketer</h3>
           <p className="text-gradient font-bold">Chief Marketing Officer</p>
           <p className="mt-4 text-center">
             The marketing guy. Done impressive and extensive research and claims to have the best
             marketing recipe to push us beyond earth's orbit. We can't wait to see that happen!
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <StaticImage
+            src="../images/joey.png"
+            quality={90}
+            className="w-[150px] mb-4"
+            alt=""
+            placeholder="none"
+          />
+          <h3 className="text-xl font-bold">@GloryDogeDev</h3>
+          <p className="text-gradient font-bold">CEO & Lead Dev</p>
+          <p className="mt-4 text-center">
+            Responsible for building, testing, and maintaining code for the contracts and the
+            upcoming platforms. Been through a successful career as a software engineer since 2014.
           </p>
         </div>
 
@@ -51,7 +51,7 @@ const TeamSection = () => {
             alt=""
             placeholder="none"
           />
-          <h3 className="text-xl font-bold">Mr. Light</h3>
+          <h3 className="text-xl font-bold">@GloryDogeDesigner</h3>
           <p className="text-gradient font-bold">Chief Design Officer</p>
           <p className="mt-4 text-center">
             The lead designer. Responsible for making everything we build look like candy to the
