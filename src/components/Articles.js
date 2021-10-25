858bd877ffcccdd887ea5de95cec00e9ab8841d9
MrJoey-GloryDoge <hello@glorydogecoin.com>
2021-10-25 10:48:17 +0200

    Update articles section

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -11,7 +11,7 @@ const Articles = () => {
         <div className="h-[2px] right-[110%] w-[50px] sm:w-[100px] lg:w-[260px] absolute bg-gradient-to-l rounded from-secondary to-orange" />
         <div className="h-[2px] left-[110%] w-[50px] sm:w-[100px] lg:w-[260px] absolute bg-gradient-to-r rounded from-secondary to-orange" />
       </div>
-      <div className="sm:w-[80%] grid gap-4 grid-cols-2 mx-auto sm:gap-8 sm:grid-cols-2 lg:grid-cols-4">
+      <div className="sm:w-[80%] grid gap-4 grid-cols-2 mx-auto sm:gap-8 sm:grid-cols-2 lg:grid-cols-3">
         <a
           className="h-[80px] sm:h-[120px] flex items-center justify-center px-2 w-full bg-white rounded-md hover:shadow-lg overflow-hidden transition"
           href="https://finance.yahoo.com/news/glorydoge-launches-hub-sharing-investing-085900976.html"
@@ -56,48 +56,6 @@ const Articles = () => {
         </a>
         <a
           className="h-[80px] sm:h-[120px] flex items-center justify-center px-2 w-full bg-white rounded-md hover:shadow-lg overflow-hidden transition"
-          href="https://bitcourier.co.uk/news/glorydoge-interview"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <StaticImage
-            src="../images/bitcourier.png"
-            alt="We aim to build a secure DeFi ecosystem to help bring creative ideas to life and protect early investors' funds, says the Founder of GloryDoge"
-            quality="90"
-            placeholder="none"
-            className="w-[160px]"
-          />
-        </a>
-        <a
-          className="h-[80px] disabled sm:h-[120px] flex items-center justify-center px-2 w-full bg-white rounded-md hover:shadow-lg overflow-hidden transition"
-          href="#"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <StaticImage
-            src="../images/digital-journal.png"
-            alt="GloryDoge Launches a Hub for Sharing, Investing, and Developing Innovative Ideas"
-            quality="90"
-            placeholder="none"
-            className="w-[160px]"
-          />
-        </a>
-        <a
-          className="h-[80px] disabled sm:h-[120px] flex items-center justify-center px-2 w-full bg-white rounded-md hover:shadow-lg overflow-hidden transition"
-          href="#"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <StaticImage
-            src="../images/big-news-network.png"
-            alt="GloryDoge Launches a Hub for Sharing, Investing, and Developing Innovative Ideas"
-            quality="90"
-            placeholder="none"
-            className="w-[160px]"
-          />
-        </a>
-        <a
-          className="h-[80px] sm:h-[120px] flex items-center justify-center px-2 w-full bg-white rounded-md hover:shadow-lg overflow-hidden transition"
           href="https://menafn.com/1103029077/GloryDoge-Launches-a-Hub-for-Sharing-Investing-and-Developing-Innovative-Ideas"
           target="_blank"
           rel="noreferrer"
@@ -111,17 +69,31 @@ const Articles = () => {
           />
         </a>
         <a
-          className="h-[80px] disabled sm:h-[120px] flex items-center justify-center px-2 w-full bg-white rounded-md hover:shadow-lg overflow-hidden transition"
-          href="#"
+          className="h-[80px] sm:h-[120px] flex items-center justify-center px-2 w-full bg-white rounded-md hover:shadow-lg overflow-hidden transition"
+          href="https://bitcourier.co.uk/news/glorydoge-interview"
           target="_blank"
           rel="noreferrer"
         >
           <StaticImage
-            src="../images/finanznachrichten.png"
+            src="../images/bitcourier.png"
+            alt="We aim to build a secure DeFi ecosystem to help bring creative ideas to life and protect early investors' funds, says the Founder of GloryDoge"
+            quality="90"
+            placeholder="none"
+            className="w-[160px]"
+          />
+        </a>
+        <a
+          className="h-[80px] sm:h-[120px] flex items-center justify-center px-2 w-full bg-white rounded-md hover:shadow-lg overflow-hidden transition"
+          href="https://www.crypto-reporter.com/newsfeed/glorydoge-launches-a-hub-for-sharing-investing-and-developing-innovative-ideas-19720"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <StaticImage
+            src="../images/crypto-reporter.png"
             alt="GloryDoge Launches a Hub for Sharing, Investing, and Developing Innovative Ideas"
             quality="90"
             placeholder="none"
-            className="w-[200px]"
+            className="w-[160px]"
           />
         </a>
       </div>
