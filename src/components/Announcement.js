7164199f4e559818b9de2487ae08fed50a7c4e2b
MrJoey-GloryDoge <hello@glorydogecoin.com>
2021-11-04 20:03:59 +0100

    Update announcement

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -8,23 +8,16 @@ const Announcement = () => {
     <Container>
       <div className="mt-16 p-5 w-full text-sm bg-secondary bg-opacity-30 border border-secondary rounded-lg space-y-2 sm:text-base">
         <p>
-          Private sale is concluded! You will be able to claim your tokens right after the launch on
-          PancakeSwap{' '}
-          <Link to="/private-sale" className="inline-block underline font-bold">
-            here.
-          </Link>
-        </p>
-        <p>
-          Presale successfully done. Launch on PancakeSwap is on October 29 at 4PM UTC. You can
-          claim your presale tokens on{' '}
+          We're airdroping $20,000 BUSD to 4 winners.{' '}
           <a
-            className="underline font-bold"
-            href="https://www.pinksale.finance/#/launchpad/0x1c5dd98f9518F755eEeB6af0F162638b756D5771?chain=BSC"
+            className="underline"
+            href="https://forms.gle/V5FxDaqtsHnCTxcL7"
             target="_blank"
             rel="noreferrer"
           >
-            PinkSale.
-          </a>
+            Participate here!
+          </a>{' '}
+          🏆
         </p>
       </div>
     </Container>
