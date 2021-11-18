e09df3282e3e4a30b5d895635cb32930b0ca709e
spacens <chao.lin@toptal.com>
2021-11-18 05:20:44 -0800

    Make responsive reflection tracker page

--- src/components/ReflectionTracker.js
+++ src/components/ReflectionTracker.js
@@ -211,20 +211,20 @@ const ReflectionTracker = () => {
         <h1 className="text-2xl font-bold leading-relaxed sm:text-4xl">Check your reflections</h1>
         <p className="text-center">Find out how much reflections you got so far</p>
 
-        <div className="sm:w-[1014px] relative flex flex-col items-center w-full bg-darkBlue rounded-lg shadow-md overflow-hidden">
+        <div className="md:w-[1014px] relative flex flex-col items-center w-full bg-darkBlue rounded-lg shadow-md overflow-hidden">
           <div className="flex flex-col items-center p-5 w-full">
-            <p className="text-center">Past your GLORYD tokens holding wallet below</p>
+            <p className="text-center">Paste your GLORYD tokens holding wallet below</p>
           </div>
-          <div className="w-[600px] flex flex-col items-center">
+          <div className="flex flex-col items-center">
             <input
               placeholder="0xXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-              className="placeholder-[#3A6D8D] focus:placeholder-transparent h-[40px] text-[#3A6D8D] w-full text-center bg-white rounded-md focus:outline-none transition duration-500"
+              className="placeholder-[#3A6D8D] focus:placeholder-transparent h-[40px] text-[#3A6D8D] w-[300px] sm:w-[400px] md:w-[600px] text-center bg-white rounded-md focus:outline-none transition duration-500"
               value={token || ''}
               onChange={e => setToken(e.target.value || '')}
             />
           </div>
           {loading && <div className="text-center">Loading ...</div>}
-          <div className="grid gap-x-24 grid-cols-2 mt-5">
+          <div className="grid gap-x-24 grid-cols-1 mt-5 sm:grid-cols-1 md:grid-cols-2">
             <Input
               title="Your balance"
               value={values.balance}
@@ -247,18 +247,24 @@ const ReflectionTracker = () => {
             />
           </div>
           <div className="relative flex items-center justify-center w-full">
-            <div className="absolute z-10 flex items-center justify-center">
+            <div className="absolute z-10 flex flex-col items-center justify-center mt-10 sm:flex-col md:flex-row md:mt-0">
               {calcPro !== 0 && (
-                <div className="mr-10">
+                <div className="md:mr-10">
                   <p className="font-extrabold">{calcPro}%</p>
-                  <p className="border-fuchsia-400 border-t-2">reflections</p>
+                  <p className="from-[#F16C28] to-[#8C2485] text-transparent bg-gradient-to-r bg-clip-text border-t-2 border-white">
+                    reflections
+                  </p>
                 </div>
               )}
               <div id="pie-container" />
               {calcPro !== 0 && (
-                <div className="ml-10">
-                  <p className="border-fuchsia-400 border-b-2">Total GLORYD</p>
-                  <p>Purchased</p>
+                <div className="md:ml-10">
+                  <p className="from-[#0C7FEC] to-[#49BEFF] text-transparent bg-gradient-to-r bg-clip-text">
+                    Total GLORYD
+                  </p>
+                  <p className="from-[#0C7FEC] to-[#49BEFF] text-transparent bg-gradient-to-r bg-clip-text border-t-2 border-white">
+                    purchased
+                  </p>
                 </div>
               )}
             </div>
@@ -268,7 +274,7 @@ const ReflectionTracker = () => {
               alt="GloryDoge Launches a Hub for Sharing, Investing, and Developing Innovative Ideas"
               quality="90"
               placeholder="none"
-              className="h-[320px] w-full"
+              className="h-[320px] mt-32 w-full md:mt-0"
             />
           </div>
         </div>
